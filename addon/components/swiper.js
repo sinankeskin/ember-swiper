52a98b9258ee6d5c193c7a59e9dcf79db8a5bca2
Sinan Keskin <sinan@kesk.in>
2020-05-31 16:11:18 +0300

    Fix and some refactor

--- addon/components/swiper.js
+++ addon/components/swiper.js
@@ -1,11 +1,15 @@
 import { getOwner } from '@ember/application';
 import { assign } from '@ember/polyfills';
-import { action, computed, get } from '@ember/object';
+import { action, computed } from '@ember/object';
 import { guidFor } from '@ember/object/internals';
+import { tracked } from '@glimmer/tracking';
 import Component from '@glimmer/component';
 
 export default class SwiperComponent extends Component {
   elementId = guidFor(this);
+
+  @tracked
+  swiper;
 
   @computed
   get _config() {
@@ -14,20 +18,21 @@ export default class SwiperComponent extends Component {
     return config['ember-swiper5'] || {};
   }
 
-  @computed('_config')
+  @computed('_componentOptions', '_config')
   get _options() {
     const options = {};
 
-    assign(options, this._config, this._componentOptions());
+    assign(options, this._config, this._componentOptions);
 
     return options;
   }
 
-  _componentOptions() {
+  @computed('args')
+  get _componentOptions() {
     const options = {};
 
     Object.keys(this.args).forEach((option) => {
-      const _option = get(this.args, option);
+      const _option = this.args[option];
 
       if (typeof _option === 'object') {
         options[option] = Object.assign({}, _option);
@@ -99,7 +104,7 @@ export default class SwiperComponent extends Component {
         }
       })
       .catch((error) => {
-        console.log('Dynamic import failed. Reason:' + error);
+        console.error('Dynamic import failed. Reason:' + error);
       });
   }
 
